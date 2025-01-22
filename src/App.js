e3fc9616f4409104c68574d7057c3989d746370c
Samuel Andrew <aondopase@gmail.com>
2025-01-22 10:40:18 +0100

    Done with useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
-  console.log("We'er about to list the employees");
+  const [role, setRole] = useState('Dev');
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ?
       <>
-        <Employee />
-        <Employee />
-        <Employee />
+        <input
+          type='text' 
+          onChange={(e) => 
+            {console.log(e.target.value);
+              setRole(e.target.value)
+            }}
+        />
+
+        <Employee name="Andy" role="Intern"/>
+        <Employee name="Moses" role={role}/>
+        <Employee name="Grace"/>
       </>
     :
     (<span>You can not see the employees</span>)
--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,6 +1,11 @@
-function Employee() {
+function Employee(props) {
 
-        return <h3>Here is an Employee!</h3>
+        return(
+                <>
+                        <h3>Employee {props.name}</h3>
+                        <p>{props.role? props.role: "No role"}</p>
+                </>
+                )
 
 }
 
