7ad9a4df5975e6a23d2cf8e7b6e0e73b3cb48076
Samuel Andrew <aondopase@gmail.com>
2025-01-22 13:37:23 +0100

    Getting reading for creating Modal Box

--- src/App.js
+++ src/App.js
@@ -1,30 +1,67 @@
-import './App.css';
-import Employee from './components/Employee';
-import { useState } from 'react';
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
+import {v4 as uuidv4} from "uuid";
 
 function App() {
-  const [role, setRole] = useState('Dev');
+  const [role, setRole] = useState("Dev");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Samuel",
+      role: "Developer",
+      img: "/images/PROPHET_TB_JOSHUA_.jpg",
+    },
+    {
+      name: "Grace",
+      role: "Marketer",
+      img: "/images/PROPHET_TB_JOSHUA_.jpg",
+    },
+    {
+      name: "Amos",
+      role: "Pastor",
+      img: "/images/PROPHET_TB_JOSHUA_.jpg",
+    },
+    {
+      name: "Awa",
+      role: "Health",
+      img: "/images/PROPHET_TB_JOSHUA_.jpg",
+    },
+    {
+      name: "Blessing",
+      role: "Intern",
+      img: "/images/PROPHET_TB_JOSHUA_.jpg",
+    },
+    {
+      name: "Joy Tina",
+      role: "Lover",
+      img: "/images/PROPHET_TB_JOSHUA_.jpg",
+    },
+  ]);
   const showEmployees = true;
   return (
     <div className="App">
-      {showEmployees ?
-      <>
-        <input
-          type='text' 
-          onChange={(e) => 
-            {console.log(e.target.value);
-              setRole(e.target.value)
+      {showEmployees ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              setRole(e.target.value);
             }}
-        />
-
-        <Employee name="Andy" role="Intern"/>
-        <Employee name="Moses" role={role}/>
-        <Employee name="Grace"/>
-      </>
-    :
-    (<span>You can not see the employees</span>)
-
-      }
+          />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return(<Employee
+                key={uuidv4()}
+                name={employee.name}
+                role={employee.role}
+                img={employee.img}
+              />);
+            })}
+          </div>
+        </>
+      ) : (
+        <span>You can not see the employees</span>
+      )}
     </div>
   );
 }
