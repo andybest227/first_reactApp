0538eb99372b7308ab66bf9ed21129d49803dfd5
Samuel Andrew <aondopase@gmail.com>
2025-01-23 10:43:37 +0100

    Done with edit Modal

--- src/App.js
+++ src/App.js
@@ -1,42 +1,60 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState("Dev");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Samuel",
       role: "Developer",
       img: "/images/PROPHET_TB_JOSHUA_.jpg",
     },
     {
+      id: 2,
       name: "Grace",
       role: "Marketer",
       img: "/images/PROPHET_TB_JOSHUA_.jpg",
     },
     {
+      id: 3,
       name: "Amos",
       role: "Pastor",
       img: "/images/PROPHET_TB_JOSHUA_.jpg",
     },
     {
+      id: 4,
       name: "Awa",
       role: "Health",
       img: "/images/PROPHET_TB_JOSHUA_.jpg",
     },
     {
+      id: 5,
       name: "Blessing",
       role: "Intern",
       img: "/images/PROPHET_TB_JOSHUA_.jpg",
     },
     {
+      id: 6,
       name: "Joy Tina",
       role: "Lover",
       img: "/images/PROPHET_TB_JOSHUA_.jpg",
     },
   ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (employee.id == id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+
+    setEmployees(updatedEmployees);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -50,11 +68,13 @@ function App() {
           />
           <div className="flex flex-wrap justify-center">
             {employees.map((employee) => {
-              return(<Employee
-                key={uuidv4()}
+              return (<Employee
+                key={employee.id}
+                id={employee.id}
                 name={employee.name}
                 role={employee.role}
                 img={employee.img}
+                updateEmployee={updateEmployee}
               />);
             })}
           </div>
